a6217921959be2f8ed3d1ca549eefcfc44d9d352
Raphaël Mateescu <73947667+SmartNewt@users.noreply.github.com>
2025-01-15 10:24:19 +0100

    [ADD] Enemy got ball State

--- LightEngine/RugbyManAction.cpp
+++ LightEngine/RugbyManAction.cpp
@@ -1,6 +1,17 @@
+#include "Field.h"
 #include "RugbyManAction.h"
 
+void RugbyManAction_EnemyGotBall::Update(RugbyMan* rugbyman)
+{
+	if (mTarget == nullptr)
+		dynamic_cast<Field*>(rugbyman->GetScene())->mBallOwner;
 
+	AABB playerLane = dynamic_cast<Field*>(rugbyman->GetScene())->mLanes[rugbyman->GetLane()];
+
+	rugbyman->GoToPosition(mTarget->GetPosition().x, mTarget->GetPosition().y);
+
+	rugbyman->KeepInRect(playerLane);
+}
 
 
 /// RAPH (haut) Flav (bas)
